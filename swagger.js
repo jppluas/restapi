d2eb87d7859be5834082379f067f800e164f9216
Juan Pablo Plúas <107544861+jppluas@users.noreply.github.com>
2024-07-31 21:00:36 -0500

    render config

--- swagger.js
+++ swagger.js
@@ -5,7 +5,10 @@ const swaggerAutogen = require('swagger-autogen')()
      "title": 'REST API',
      "description": 'REST API with Express and Firestore'
    },
-   "host": 'localhost:5500',
+   "host": 'restapi-cizn.onrender.com',
+   "schemes": [
+     "https"
+   ],
     "basePath": "/api",
  };
 
